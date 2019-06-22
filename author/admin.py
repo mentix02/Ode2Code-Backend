236a8651d12ae1d2bcfd4b6a2413c8270f3aa8ab
mentix02 <manan.yadav02@gmail.com>
2019-06-22 14:03:31 +0530

    added Bookmark model to admin

--- author/admin.py
+++ author/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from author.models import Author
+from author.models import Author, Bookmark
 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('user', 'authenticated',)
 
 
+admin.site.register(Bookmark)
 admin.site.register(Author, AuthorAdmin)
