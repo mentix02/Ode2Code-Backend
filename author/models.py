55cf8520343e71a9538c5d423fc076505a8dfa21
mentix02 <manan.yadav02@gmail.com>
2019-06-22 13:58:45 +0530

    added a Bookmark model

--- author/models.py
+++ author/models.py
@@ -25,6 +25,22 @@ class Author(models.Model):
         ordering = ('-pk',)
 
 
+class Bookmark(models.Model):
+
+    CHOICES = (
+        ('series', 'Series'),
+        ('tutorial', 'Tutorial'),
+    )
+
+    model_pk = models.BigIntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    model_type = models.CharField(choices=CHOICES, max_length=8)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author} - {self.model_type} : {self.model_pk}'
+
+
 # noinspection PyUnusedLocal
 @receiver(post_save, sender=User)
 def author_generate_token(sender, instance: User = None, created=False, **kwargs):
