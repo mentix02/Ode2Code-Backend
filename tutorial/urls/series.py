7d63dca84c8fe136e55571257cab7fe351e6b4ae
mentix02 <manan.yadav02@gmail.com>
2019-07-06 01:40:24 +0530

    added views to urls

--- tutorial/urls/series.py
+++ tutorial/urls/series.py
@@ -5,16 +5,23 @@ from tutorial.views.series import (
     SeriesDetailAPIView,
     SeriesCreateAPIView,
     SeriesBookmarkAPIView,
+    SeriesTypeListAPIView,
+    SeriesAvailabilityAPIView,
+    SeriesNameAndIdListAPIView,
     SeriesTutorialsListAPIView,
 )
 
 urlpatterns = (
+
     path('', SeriesListAPIView.as_view()),
+    path('names/', SeriesNameAndIdListAPIView.as_view()),
+    path('type/<slug:slug>/', SeriesTypeListAPIView.as_view()),
     path('detail/<slug:slug>/', SeriesDetailAPIView.as_view()),
     path('detail/<slug:slug>/tutorials/', SeriesTutorialsListAPIView.as_view()),
 
     # CRUD operations on series
     path('new/', SeriesCreateAPIView.as_view()),
-    path('bookmark/', SeriesBookmarkAPIView.as_view())
+    path('bookmark/', SeriesBookmarkAPIView.as_view()),
+    path('is_available/', SeriesAvailabilityAPIView.as_view()),
 
 )
