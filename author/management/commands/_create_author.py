aec9a441c23eeb05d270a9d87f076fc054cb4cae
mentix02 <manan.yadav02@gmail.com>
2019-07-08 05:19:21 +0530

    made create_author() username unique proof

--- author/management/commands/_create_author.py
+++ author/management/commands/_create_author.py
@@ -9,21 +9,29 @@ from author.models import Author
 fake = faker.Faker()
 
 
+def generate_fake_username(username):
+    usernames = [username[0] for username in User.objects.values_list('username')]
+    if username in usernames:
+        return generate_fake_username(fake.user_name())
+    return username
+
+
 def create_user() -> typing.Tuple[int, bool]:
 
     authenticate = random.random() < 0.08
 
     name: typing.List[str] = fake.name().split()
     first_name, last_name = name[0], name[1]
+    username = generate_fake_username(fake.user_name())
 
-    user = User.objects.create(
+    user = User.objects.create_user(
+        password='aaa',
+        username=username,
         email=fake.email(),
         last_name=last_name,
         is_staff=authenticate,
         first_name=first_name,
-        username=fake.user_name(),
     )
-    user.set_password('aaaa')
     user.save()
     return user.id, authenticate
 
