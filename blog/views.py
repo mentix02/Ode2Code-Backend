2b6858347ba4bd056caa0abb8d4bfb726f8a9864
mentix02 <manan.yadav02@gmail.com>
2019-07-10 04:03:08 +0530

    added an untested view for creating blog posts

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,10 @@ TODO write a view listing blog posts for authenticated
      authentication will have to be covered much later.
 
 """
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
 from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
@@ -48,3 +51,59 @@ class PostDetailAPIView(RetrieveAPIView):
     permission_classes = (AllowAny,)
     serializer_class = PostDetailSerializer
     queryset = Post.objects.filter(draft=False)
+
+
+class PostCreateAPIView(APIView):
+    """
+    Provides an interface to create new
+    only if an authenticated request comes through.
+    """
+
+    @staticmethod
+    def post(request):
+
+        # get required params
+        try:
+            body = request.POST['body']
+            title = request.POST['title']
+            thumbnail = request.POST['thumbnail']
+            draft = request.POST.get('draft', False)
+            description = request.POST['description']
+        except Exception as e:
+            return Response({
+                'error': f'{str(e)} field not provided.'
+            })
+
+        token = request.POST.get('token')
+
+        if not token and not request.user.is_authenticated:
+            return Response({
+                'error': 'You need to be authenticated to create a new tutorial.'
+            }, status=401)
+
+        try:
+
+            if token:
+                author_id = Token.objects.get(key=token).user.author.id
+            else:
+                author_id = request.user.author.id
+
+            post = Post.objects.create(
+                body=body,
+                title=title,
+                draft=draft,
+                author_id=author_id,
+                thumbnail=thumbnail,
+                description=description
+            )
+
+            data = PostDetailSerializer(post).data
+
+            return Response({
+                'details': data
+            }, status=201)
+
+        except Exception as e:
+            return Response({
+                'error': str(e)
+            }, status=500)
