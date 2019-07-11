14772f19523b26b0d9f5def63a63f6eea302e0b6
mentix02 <manan.yadav02@gmail.com>
2019-07-11 12:49:24 +0530

    removed TODO for post create view

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,6 @@
 API views for posts in the blog models. Room for
 cache can be made with caching PostDetailAPIView.
 
-TODO creating blog posts with POST based API views.
-     Can be implemented with ListCreateAPIView but need
-     for authentication will have to be figured out.
-
 TODO write a view listing blog posts for authenticated
      users to delete or update or simply view drafts
      or posted material. Same as PostCreateAPIView,
