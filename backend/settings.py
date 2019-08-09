e3dac69fa405971c122b2535612e5d6ac65da7cb
mentix02 <manan.yadav02@gmail.com>
2019-08-09 17:20:23 +0530

    changed page size to 12 for better UI purposes

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 12,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
